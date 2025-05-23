76c8bd0b5384fdfa1a4bd7d89359d703c5990032
Vasile Pop <vasile.pop@gmail.com>
2025-05-23 18:32:01 +0300

    add page skeletons, add astro-icon package

---
+++ astro.config.mjs
@@ -0,0 +1,13 @@
+import { defineConfig } from 'astro/config';
+
+import tailwindcss from '@tailwindcss/vite';
+
+import icon from 'astro-icon';
+
+export default defineConfig({
+  integrations: [icon()],
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
+});
