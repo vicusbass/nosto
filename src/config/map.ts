9fe49cd9b221d25962d85ba49859d34967d3720e
Vasile Pop <vasile.pop@gmail.com>
2025-06-13 23:07:14 +0300

    move location details to map config object

--- src/config/map.ts
+++ src/config/map.ts
@@ -2,4 +2,26 @@ export default {
   center: [45.752179, 21.275771],
   title: 'NOSTO',
   googleMapsLink: 'https://maps.app.goo.gl/VaRfRMyJi5aHt9kYA',
+  locationDetails: [
+    {
+      icon: 'mdi:car',
+      title: 'Centru Cluj',
+      details: '5 minute',
+    },
+    {
+      icon: 'mdi:walk',
+      title: 'Centru Cluj',
+      details: '15 minute',
+    },
+    {
+      icon: 'mdi:bus',
+      title: 'Stații de transport în comun',
+      details: 'în proximitate',
+    },
+    {
+      icon: 'mdi:tree',
+      title: 'Parcuri publice',
+      details: '15 minute de mers pe jos',
+    },
+  ],
 };
