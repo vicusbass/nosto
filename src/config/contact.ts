b907bfd04a02ce82fffb509ea286ce724b8a965f
Vasile Pop <vasile.pop@gmail.com>
2025-06-19 16:08:28 +0300

    update contact

--- src/config/contact.ts
+++ src/config/contact.ts
@@ -3,4 +3,9 @@ export default {
   email: 'contact@nosto.ro',
   address: 'https://maps.app.goo.gl/VaRfRMyJi5aHt9kYA',
   whatsapp: '123456789',
+  social: {
+    facebook: 'https://www.facebook.com/nostorocluja',
+    instagram: 'https://www.instagram.com/nostorocluja',
+    tiktok: 'https://www.tiktok.com/@nostorocluja',
+  },
 };
