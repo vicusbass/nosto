6d818870ff462293dbe44c1c45ad0617ca6e38eb
Vasile Pop <vasile.pop@gmail.com>
2025-06-23 15:35:57 +0300

    vercel function for email

--- src/config/contact.ts
+++ src/config/contact.ts
@@ -1,6 +1,6 @@
 export default {
   phone: '123456789',
-  email: 'contact@nosto.ro',
+  email: 'info@nosto.ro',
   address: 'https://maps.app.goo.gl/VaRfRMyJi5aHt9kYA',
   whatsapp: '123456789',
   social: {
